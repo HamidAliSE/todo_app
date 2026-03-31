35f19834ad13b898630f8d17eca8953c24ae5d0e
Hamid Ali <hamidalig942@gmail.com>
2026-03-31 11:53:18 +0500

    update: app title to To Do App

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'To Do App',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const MyHomePage(title: 'To Do App'),
     );
