3829116da754deff42fd628a5e2fc05a64a7ab45
Hamid Ali <hamidalig942@gmail.com>
2026-03-31 11:55:51 +0500

    refactor: removed increment counter & related code

--- lib/main.dart
+++ lib/main.dart
@@ -27,12 +27,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   List<String> _tasks = [];
 
-  void _incrementCounter() {
+  void _addTask() {
     setState(() {
-      _counter++;
       _tasks.add('Task ${_tasks.length}');
     });
   }
@@ -48,11 +46,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
             ..._tasks.map((task) {
               return Text(task);
             }),
@@ -60,8 +53,8 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _addTask,
+        tooltip: 'Add Task',
         child: const Icon(Icons.add),
       ),
     );
