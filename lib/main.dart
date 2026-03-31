d851d8fd9a1aada126651a78e297ec8010e80c4b
Hamid Ali <hamidalig942@gmail.com>
2026-03-31 11:48:02 +0500

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -44,9 +44,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
             const Text('You have pushed the button this many times:'),
